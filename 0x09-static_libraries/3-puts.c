f2d73f6d5cbe248d17813d1d5f3cedf05587c2a5
Foche78 <frank@gmail.com>
2023-03-16 07:21:24 -0700

    0-malloc_checked.c

---
+++ 0x09-static_libraries/3-puts.c
@@ -0,0 +1,15 @@
+#include "main.h"
+void _puts(char *str)
+#include<stdio.h>
+/**
+ * _puts - Entry point
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
+
